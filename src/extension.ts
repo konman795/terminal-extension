dd1badfebfd82493dc07ed9430421d72d76630f1
Kon-Kon <konman795@gmail.com>
2019-05-30 23:25:16 -0400

    v0.1 beta

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	let NEXT_TERM_ID = 1;
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 		console.log('Congratulations, your extension "helloworld" is now active!');
@@ -13,31 +13,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.gulpWatch', (uri:vscode.Uri) => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
-	});
-
-	let warningMessage = vscode.commands.registerCommand('extension.warningMessage', () => {
-		// The code you place here will be executed every time your command is executed
+		const relativePath = vscode.workspace.asRelativePath(uri, true);
 
 		// Display a message box to the user
-		vscode.window.showErrorMessage('Warning Test!');
+		vscode.window.showInformationMessage(`Gulp command! ${relativePath}`);
+
+		const terminal = vscode.window.createTerminal(`Gulp Watch ${NEXT_TERM_ID}`);
+		terminal.show(true);
+		terminal.sendText(`echo 'Gulp watching ${relativePath}'`);
+		NEXT_TERM_ID++;
 	});
 
-	let displayTime = vscode.commands.registerCommand('extension.displayTime', () => {
-		// The code you place here will be executed every time your command is executed
-		const today = new Date();
-		const currentTime = `${today.getHours()}:${today.getMinutes()}`;
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage(currentTime);
-	});
-
-	context.subscriptions.push(disposable, displayTime, warningMessage);
+	context.subscriptions.push(disposable);
 }
-
 // this method is called when your extension is deactivated
 export function deactivate() {}
