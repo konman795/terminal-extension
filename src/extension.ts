f508e89baf3b9f498b91d4ca3882269e1fdb6c66
Kon-Kon <schao@redventures.com>
2019-05-31 14:36:07 -0400

    Add staging and production deploy items

--- src/extension.ts
+++ src/extension.ts
@@ -13,21 +13,33 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.gulpWatch', (uri:vscode.Uri) => {
+	let gulpCommand = vscode.commands.registerCommand('extension.gulpWatch', (uri:vscode.Uri) => {
 		// The code you place here will be executed every time your command is executed
 
-		const relativePath = vscode.workspace.asRelativePath(uri, true);
+		const relativePath = vscode.workspace.asRelativePath(uri, false);
+		const workspaceFolder = vscode.workspace.getWorkspaceFolder(uri);
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage(`Gulp command! ${relativePath}`);
+		vscode.window.showInformationMessage(`Watching ${workspaceFolder}`);
+
+		console.log(workspaceFolder);
+		console.log(uri);
 
 		const terminal = vscode.window.createTerminal(`Gulp Watch ${NEXT_TERM_ID}`);
 		terminal.show(true);
-		terminal.sendText(`echo 'Gulp watching ${relativePath}'`);
+		terminal.sendText(`gulp watch --preamp --asset='${relativePath}'`);
 		NEXT_TERM_ID++;
 	});
 
-	context.subscriptions.push(disposable);
+	let deployStagingCommand = vscode.commands.registerCommand('extension.deployStaging', (uri: vscode.Uri) => {
+		vscode.window.showInformationMessage(`Deploy to staging?`);
+	});
+
+	let deployProdCommand = vscode.commands.registerCommand('extension.deployProduction', (uri: vscode.Uri) => {
+		vscode.window.showInformationMessage(`Deploy to production?`);
+	});
+
+	context.subscriptions.push(gulpCommand, deployStagingCommand, deployProdCommand);
 }
 // this method is called when your extension is deactivated
 export function deactivate() {}
